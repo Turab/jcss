72e7139d320ae734f0b407e610f3e852065fdc04
Turab <ozhan@elmacik.com>
2021-05-11 17:12:35 +0300

    Update jcss.php

--- jcss.php
+++ jcss.php
@@ -7,15 +7,6 @@
 *  GNU GPL v3.0
 *******************************************************************/
 
-// Either include a config file where your environment settings are defined, or define them here.
-$env = array(
-	'cache_jcss' => true, // Cache combined CSS & JS files for performance
-	'compress_jcss' => true, // Compress CSS & JSS files with gzip to lower HTTP traffic
-	'cache' => '/tmp/cache', // Where to store cache files
-	'path' => '/var/www/vhosts/example.com/html', // Your base path
-	'template' => '/var/www/vhosts/example.com/html/templates/nightwolf', // Your template files
-);
-
 // This file can be cached yes.
 header('Cache-Control: public');
 
