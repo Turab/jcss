9041d4e3d01745240b76fcecb595843ef517aee6
Turab <ozhan@elmacik.com>
2021-05-11 17:13:33 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$env = array(
+	'cache_jcss' => true, // Cache combined CSS & JS files for performance
+	'compress_jcss' => true, // Compress CSS & JSS files with gzip to lower HTTP traffic
+	'cache' => '/tmp/cache', // Where to store cache files
+	'path' => '/var/www/vhosts/example.com/html', // Your base path
+	'template' => '/var/www/vhosts/example.com/html/templates/nightwolf', // Your template files
+  'base' => 'https://example.com'; // You sites base URL
+);
